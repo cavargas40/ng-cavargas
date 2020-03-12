30ccd018c825378d0792b24e7e4ad5087171b447
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-03-11 21:36:50 -0500

    nav-user in navigation bar

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -4,15 +4,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavItemComponent } from './navbar/nav-item/nav-item.component';
+import { NavUserComponent } from './navbar/nav-user/nav-user.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, NavItemComponent, NavUserComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule
   ],
-  exports: [NavbarComponent]
+  exports: [NavbarComponent, NavUserComponent]
 })
 export class CoreModule {}
