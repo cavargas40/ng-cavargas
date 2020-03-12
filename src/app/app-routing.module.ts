b457bb2362f30e896db25488fb52813c66a9ac96
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-03-12 00:20:37 -0500

    added pace loader, modules for all the links & firebase hosting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,26 @@ const routes: Routes = [
       import('./modules/home/home.module').then(m => m.HomeModule)
   },
   {
+    path: 'about-me',
+    loadChildren: () =>
+      import('./modules/about-me/about-me.module').then(m => m.AboutMeModule)
+  },
+  {
+    path: 'resume',
+    loadChildren: () =>
+      import('./modules/resume/resume.module').then(m => m.ResumeModule)
+  },
+  {
+    path: 'links',
+    loadChildren: () =>
+      import('./modules/links/links.module').then(m => m.LinksModule)
+  },
+  {
+    path: 'contact',
+    loadChildren: () =>
+      import('./modules/contact/contact.module').then(m => m.ContactModule)
+  },
+  {
     path: '**',
     redirectTo: 'home' //TODO: 404 page
   }
--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+
+import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
+
+import { NavbarComponent } from './navbar/navbar.component';
 import { NavItemComponent } from './navbar/nav-item/nav-item.component';
 import { NavUserComponent } from './navbar/nav-user/nav-user.component';
 
@@ -15,7 +18,8 @@ import { NavUserComponent } from './navbar/nav-user/nav-user.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatIconModule
+    MatIconModule,
+    RouterModule
   ],
   exports: [NavbarComponent, NavUserComponent, NavItemComponent]
 })
