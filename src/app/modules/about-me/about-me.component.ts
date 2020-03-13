b0c18267271f94fb90c248954a52322413f70584
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-03-13 01:34:37 -0500

    auto update my age in about me

--- src/app/modules/about-me/about-me.component.ts
+++ src/app/modules/about-me/about-me.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent implements OnInit {
-
-  constructor() { }
+  public age: number = 0;
+  constructor() {}
 
   ngOnInit(): void {
+    const dateOfBirth: Date = new Date('10/14/1991');
+    this.age = this.calcualteAge(dateOfBirth);
   }
 
+  calcualteAge(dateOfBirth: Date) {
+    const ageDifMs = Date.now() - dateOfBirth.getTime();
+    const ageDate = new Date(ageDifMs);
+
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
 }
