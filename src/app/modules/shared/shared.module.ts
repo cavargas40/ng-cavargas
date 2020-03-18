1377c11e9cf0ee92287cc1d5fa11f70a935034aa
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-03-17 21:26:43 -0500

    progressbar to create skill bar

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 
 import { MatDividerModule } from '@angular/material/divider';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 import { UnderConstructionComponent } from './under-construction/under-construction.component';
 import { DividerComponent } from './divider/divider.component';
+import { SkillBarComponent } from './skill-bar/skill-bar.component';
 
 @NgModule({
-  declarations: [UnderConstructionComponent, DividerComponent],
-  imports: [MatDividerModule],
+  declarations: [UnderConstructionComponent, DividerComponent, SkillBarComponent],
+  imports: [MatDividerModule, MatProgressBarModule],
   exports: [UnderConstructionComponent, DividerComponent]
 })
 export class SharedModule {}
